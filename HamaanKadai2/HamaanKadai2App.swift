04233ec7feaaf8757d96a962020ac418a4c7ebcb
hamasakiryo <hamaan.0927@icloud.com>
2024-01-22 22:43:07 +0900

    Initial Commit

---
+++ HamaanKadai2/HamaanKadai2App.swift
@@ -0,0 +1,17 @@
+//
+//  HamaanKadai2App.swift
+//  HamaanKadai2
+//
+//  Created by 浜崎良 on 2024/01/22.
+//
+
+import SwiftUI
+
+@main
+struct HamaanKadai2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
